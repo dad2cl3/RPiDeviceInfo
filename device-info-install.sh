7b0f2a02982bbeb6ce631ebf5ffa4648a53275a6
Jason Chalmers <jason@dad2cl3.com>
2020-10-12 15:06:48 -0400

    First commit

---
+++ device-info-install.sh
@@ -0,0 +1,12 @@
+# create symlink from local project service file to systemd
+sudo ln -sf /var/pyapps/device/device-info.service /etc/systemd/system/device-info.service
+
+# reload the systemd daemon
+# NOTE: any change to the local project service file will require a reload
+sudo systemctl daemon-reload
+
+# enable the linux service
+sudo systemctl enable device-info.service
+
+# start the linux service
+sudo systemctl start device-info.service
---
+++ device_info.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from gpiozero import PingServer
+from psutil import cpu_percent, sensors_temperatures, virtual_memory
+from pytz import timezone
+from socket import gethostname
+from time import sleep
+import json, paho.mqtt.publish as publish
+
+
+def network_status():
+    ping = PingServer('google.com')
+    return ping.value
+
+
+def mqtt_publish_single(message):
+
+    publish.single(
+        topic='{0}/telemetry/'.format(hostname),
+        payload=json.dumps(message),
+        hostname='192.168.0.80',
+        port=1883
+    )
+
+
+# set the timezone
+tz = timezone('America/New_York')
+# set the date time format for telemetry readings
+date_time_format = '%Y-%m-%d %H-%M-%S %z'
+# get the hostname for telemetry readings
+hostname = gethostname()
+
+while True:
+    now = tz.localize(datetime.now())
+    now_ts = now.strftime(date_time_format)
+
+    sensors = sensors_temperatures()
+    for name, entries in sensors.items():
+        if name == 'cpu_thermal':
+            temp = entries[0].current
+
+    cpu_load = cpu_percent(interval=1)
+    mem_load = virtual_memory().percent
+
+    print('CPU Temperature {0}'.format(temp))
+    print('CPU Load Average {0}'.format(cpu_load))
+    print('Memory Load Average {0}'.format(mem_load))
+
+    if network_status():
+        mqtt_publish_single({
+            'timestamp': now_ts,
+            'hostname': hostname,
+            'temperature': temp,
+            'cpu_load': cpu_load,
+            'memory_load': mem_load
+        })
+
+    sleep(60)
