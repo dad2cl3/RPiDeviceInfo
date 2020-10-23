f7e52ace9a1c60cdfa7acec2a5c351232efe757a
Jason Chalmers <jason@dad2cl3.com>
2020-10-23 09:49:12 -0400

    Added disk usage

--- device_info.py
+++ device_info.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from gpiozero import PingServer
+from gpiozero import DiskUsage, PingServer
 from psutil import cpu_percent, sensors_temperatures, virtual_memory
 from pytz import timezone
 from socket import gethostname
@@ -8,7 +8,7 @@ import json, paho.mqtt.publish as publish
 
 
 def network_status():
-    ping = PingServer('google.com')
+    ping = PingServer(config['ping_target'])
     return ping.value
 
 
@@ -17,9 +17,7 @@ def mqtt_publish_single(message):
     publish.single(
         topic='{0}/telemetry/'.format(hostname),
         payload=json.dumps(message),
-        # hostname='localhost',
         hostname=config['mqtt']['address'],
-        # port=1883
         port=config['mqtt']['port']
     )
 
@@ -29,10 +27,8 @@ with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
 # set the timezone
-# tz = timezone('America/New_York')
 tz = timezone(config['timezone'])
 # set the date time format for telemetry readings
-# date_time_format = '%Y-%m-%d %H-%M-%S %z'
 date_time_format = config['date_time_format']
 # get the hostname for telemetry readings
 hostname = gethostname()
@@ -48,10 +44,12 @@ while True:
 
     cpu_load = cpu_percent(interval=1)
     mem_load = virtual_memory().percent
+    disk_usage = DiskUsage()
 
     print('CPU Temperature {0}'.format(temp))
     print('CPU Load Average {0}'.format(cpu_load))
     print('Memory Load Average {0}'.format(mem_load))
+    print('Disk Usage {0}'.format(disk_usage))
 
     if network_status():
         mqtt_publish_single({
@@ -59,7 +57,8 @@ while True:
             'hostname': hostname,
             'temperature': temp,
             'cpu_load': cpu_load,
-            'memory_load': mem_load
+            'memory_load': mem_load,
+            'disk_usage': disk_usage
         })
 
     sleep(60)
